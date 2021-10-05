a37cb06264eb99f8fe092da7f1523423c341826b
Sean Luo <shawnluo415@gmail.com>
2021-10-05 11:18:14 -0700

    d

---
+++ Models/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Code1st.Models
+{
+    public class Player
+    {
+        public int PlayerId { get; set; } // EF will recognize this as Key, with either "PlayerId, or id"
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Position { get; set; }
+
+        // We don't say FK cuz we should only think in code.
+        public string TeamName { get; set; }
+        public Team Team { get; set; } // This obj provides the navigation back to the key. 
+
+
+        public Player()
+        {
+        }
+    }
+}
---
+++ Models/Team.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Code1st.Models
+{
+    public class Team
+    {
+        [Key] //set this as key since its unique
+        public string TeamName { get; set; }
+
+        public string City { get; set; }
+
+        public List<Player> Players { get; set; } // A team has a list of players.
+
+        public Team()
+        {
+
+        }
+    }
+}
